c3ac0ba4c6e93d1242cc09eefa4de5845b267daa
Luiz Felipe <luizfelipesralves@gmail.com>
2023-05-11 08:57:11 -0300

    Add new class Terceirizado

---
+++ classes/Terceirizado.class.php
@@ -0,0 +1,45 @@
+<?php
+    require_once("Conexao.class.php");
+
+    class Terceirizados{
+        private $con;
+        private $nome;
+        private $email;
+        private $senha;
+        private $telefone;
+
+    public function __construct(){
+        $this->con = new Conexao();
+    }
+
+        function editarPerfilTerceirizado($codigo,$nome,$email,$telefone){
+            try{
+                $this->codigo = $codigo;
+                $this->nome = $nome;
+                $this->email = $email;
+                $this->telefone = $telefone;
+        
+                $query = $this->con->conectar()->prepare("SELECT * FROM terceirizado WHERE cod = ?");
+                $query->bindParam(1,$this->codigo);
+                $query->execute();
+                $retorno = $query->fetch(PDO::FETCH_ASSOC);
+                if(count($retorno) > 0){
+                    $query = $this->con->conectar()->prepare("UPDATE terceirizado SET nome = ?, email = ?, telefone = ? WHERE cod = ?");
+                    $query->bindParam(1, $nome);
+                    $query->bindParam(2, $email);
+                    $query->bindParam(7, $telefone);
+                    $query->bindParam(8, $codigo);
+
+                    $retorno = $query->execute();//retorno boolean padrao TRUE
+                    if($retorno){
+                        return 1;
+                    } else{
+                        return 0;
+                    }        
+                }
+            } catch(PDOException $ex){
+            return 'error'.$ex->getMessage();
+            }
+        }
+    }
+?>
