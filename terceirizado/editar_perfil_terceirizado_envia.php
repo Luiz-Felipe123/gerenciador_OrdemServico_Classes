eb0c5b24817f0fe6208b4d93e66e67e945c264df
Luiz Felipe <luizfelipesralves@gmail.com>
2023-05-12 20:55:32 -0300

    change how to edit perfil Terceirizado with classes

--- terceirizado/editar_perfil_terceirizado_envia.php
+++ terceirizado/editar_perfil_terceirizado_envia.php
@@ -1,14 +1,15 @@
 <?php
 require_once("../valida_session/valida_session.php");
-require_once ("../bd/bd_terceirizado.php");
+require_once ("../classes/Terceirizado.class.php");
          
 $codigo = $_POST["cod"];
 $nome = $_POST["nome"];
 $email = $_POST["email"];
 $telefone = $_POST["telefone"];
-$data=date("y/m/d");
 
-$dados = editarPerfilTerceirizado($codigo,$nome,$email,$telefone,$data);
+$objTer = new Terceirizados();
+
+$dados = $objTer->editarPerfilTerceirizado($codigo,$nome,$email,$telefone);
 if ($dados == 1){
     $_SESSION['nome_usu'] = $nome;
     $_SESSION['texto_sucesso'] = 'Os dados do terceirizado foram alterados no sistema.';
